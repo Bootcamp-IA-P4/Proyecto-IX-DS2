f4d0200163b5973681db0b8cfe62b2c43ada0459
Omar Lengua <omarns21@gmail.com>
2025-06-18 18:00:05 +0200

    feat: fixing error, adding env prod and dev, other features.

--- client/src/services/api.js
+++ client/src/services/api.js
@@ -1,7 +1,9 @@
 import axios from 'axios';
 
+const baseURL = import.meta.env.VITE_API_BASE_URL;
+
 const apiClient = axios.create({
-  baseURL: import.meta.env.VITE_API_BASE_URL,
+  baseURL: baseURL,
   headers: {
     'Content-Type': 'application/json',
   },
@@ -15,24 +17,17 @@ export const getHistory = () => {
   return apiClient.get('/all-predicts');
 };
 
-
 export const predictWithImage = async (imageFile) => {
   const formData = new FormData();
-  formData.append('file', imageFile); 
+  formData.append('file', imageFile);
 
   try {
-    const response = await apiClient.post(
-      '/predict-image',
-      formData,
-      {
-        headers: {
-          'Content-Type': 'multipart/form-data',
-        },
-      }
-    );
-    
+    const response = await apiClient.post('/predict-image', formData, {
+      headers: {
+        'Content-Type': 'multipart/form-data',
+      },
+    });
     return response.data;
-
   } catch (error) {
     console.error("Error en la llamada a predictWithImage:", error.response?.data || error.message);
     throw error.response?.data || new Error("Error de red o del servidor");
